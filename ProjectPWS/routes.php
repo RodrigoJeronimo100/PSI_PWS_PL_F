2ec5811e1c3eede4df1c5ab0cda3e39b0e241305
RodrigoJeronimo100 <114529759+RodrigoJeronimo100@users.noreply.github.com>
2023-05-29 22:07:11 +0100

    bug fix na pasta public

---
+++ ProjectPWS/routes.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'controllers/AuthController.php';
+require_once 'controllers/PlanoController.php';
+require_once 'controllers/HomeController.php';
+require_once 'controllers/BookController.php';
+require_once 'controllers/ChapterController.php';
+
+return [
+    'defaultRoute' => ['GET', 'HomeController', 'index'],
+    'auth' => [
+        'index' => ['GET', 'AuthController', 'index'],
+        'login' => ['POST', 'AuthController', 'login'],
+        'logout'=>['GET','AuthController','logout'],
+    ],
+    'plano'=>[
+        'index'=> ['GET','PlanoController','index'],
+        'show' => ['POST','PlanoController','show']
+    ],
+    'book'=>[
+        'index'=> ['GET', 'BookController', 'index'],
+        'show' => ['GET', 'BookController', 'show'],
+        'create' => ['GET', 'BookController', 'create'],
+        'store' => ['POST', 'BookController', 'store'],
+        'edit' => ['GET', 'BookController', 'edit'],
+        'update' => ['POST', 'BookController', 'update'],
+        'delete' => ['GET', 'BookController', 'delete']
+    ],
+    'chapter'=> [
+        'index' => ['GET', 'ChapterController', 'index'],
+        'create' => ['GET', 'ChapterController', 'create'],
+        'store' => ['POST', 'ChapterController', 'store'],
+        'edit' => ['GET', 'ChapterController', 'edit'],
+
+]
+];
